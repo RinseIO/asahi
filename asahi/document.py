a7ea3a3db221b1fbc51a6ec65a83a501dfe96b7f
Kelp <kelp@phate.org>
2014-05-20 16:39:46 +0800

    updated Document

--- asahi/document.py
+++ asahi/document.py
@@ -1,8 +1,21 @@
 from couchdbkit.schema import DocumentBase
+from couchdbkit.ext.django.schema import DocumentMeta
+from couchdbkit.ext.django.loading import get_db
 from query import Query
 
 
 class Document(DocumentBase):
+    __metaclass__ = DocumentMeta
+
+    @classmethod
+    def get_db(cls):
+        db = getattr(cls, '_db', None)
+        if db is None:
+            app_label = getattr(cls._meta, "app_label")
+            db = get_db(app_label)
+            cls._db = db
+        return db
+
     @classmethod
     def where(cls, member, **kwargs):
         """
