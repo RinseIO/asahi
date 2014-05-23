b407e69c690527486a6a39f891331889502ce2c5
Kelp <kelp@phate.org>
2014-05-23 17:41:12 +0800

    added tests for asahi.properties

--- tests/test_asahi_utils.py
+++ tests/test_asahi_utils.py
@@ -4,12 +4,12 @@ from tests import patcher
 from asahi import utils
 
 
-class TestUtils(unittest.TestCase):
+class TestAsahiUtils(unittest.TestCase):
     @patcher(
         patch('asahi.utils.get_elasticsearch_url', new=MagicMock(return_value='http://localhost:9200')),
         patch('elasticsearch.Elasticsearch', new=MagicMock(return_value='es')),
     )
-    def test_utils_get_elasticsearch(self):
+    def test_asahi_utils_get_elasticsearch(self):
         from elasticsearch import Elasticsearch
         es = utils.get_elasticsearch()
         Elasticsearch.assert_called_with('localhost', port=9200)
@@ -18,6 +18,6 @@ class TestUtils(unittest.TestCase):
     @patcher(
         patch('django.conf.settings.ELASTICSEARCH_URL', new_callable=MagicMock(return_value='es')),
     )
-    def test_utils_get_elasticsearch_url(self):
+    def test_asahi_utils_get_elasticsearch_url(self):
         url = utils.get_elasticsearch_url()
         self.assertEqual(url, 'es')
