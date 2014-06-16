5bc3638c045338a31fbb05f2846bc6940433b9ae
Kelp <kelp@phate.org>
2014-06-16 18:56:55 +0800

    Revert "updated test.py"
    This reverts commit 9cc8f9beb52cb927ca524a18a63b60cb4d83c1ec.

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import unittest, sys, os
+import unittest
 from django.conf import settings
 
 
@@ -6,14 +6,6 @@ if __name__ == '__main__':
     settings.configure(
         ELASTICSEARCH_URL='http://localhost:9200',
     )
-
-    # extend libs
-    base_dir = os.path.dirname(os.path.realpath(__file__))
-    sys.path.extend([
-        base_dir,
-        os.path.join(base_dir, 'submodules', 'couchdbkit'),
-        os.path.join(base_dir, 'submodules', 'elasticsearch-py'),
-    ])
 
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
