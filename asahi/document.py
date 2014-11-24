4d49387ccf2cfc08f361be39ca82b8c320f6de05
Kelp <kelp@phate.org>
2014-11-24 16:54:58 +0800

    [tmp] update Document and Property

--- asahi/document.py
+++ asahi/document.py
@@ -7,42 +7,62 @@ from couchdbkit.ext.django.schema import DocumentMeta
 from couchdbkit.ext.django.loading import get_db
 from query import Query
 import utils
+from .properties import Property, StringProperty, LongProperty
 
 
-class Document(DocumentBase):
-    __metaclass__ = DocumentMeta
+class Document(object):
+    """
+    :attribute _document: {dict} {'property_name': (value)}
+    :attribute _properties: {dict} {'property_name': {Property}}
+    """
+    _id = StringProperty()
+    _version = LongProperty()
 
-    @classmethod
-    def get_db(cls):
-        db = getattr(cls, '_db', None)
-        if db is None:
-            app_label = getattr(cls._meta, "app_label")
-            db = get_db(app_label)
-            cls._db = db
-        return db
+    def __new__(cls, *args):
+        cls._properties = {}
+        for attribute_name in dir(cls):
+            if attribute_name.startswith('__'):
+                continue
+            attribute = getattr(cls, attribute_name)
+            if isinstance(attribute, Property):
+                cls._properties[attribute_name] = attribute
+                attribute.__property_config__(cls, attribute_name)
+        return object.__new__(cls, *args)
 
-    @classmethod
-    def get(cls, ids, rev=None, db=None, dynamic_properties=True):
-        """
-        Get documents by ids.
-        :param ids: {list or string} The documents' id.
-        :return: {list or Document}
-        """
-        if ids is None:
-            return None
-        if isinstance(ids, list):
-            if db is None:
-                db = cls.get_db()
-            return db.view(
-                '%s/id' % db.dbname,
-                keys=ids,
-                schema=cls
-            ).all()
-        else:
-            try:
-                return super(Document, cls).get(ids, rev, db, dynamic_properties)
-            except ResourceNotFound:
-                return None
+    def __init__(self, **kwargs):
+        super(Document, self).__init__()
+        self._document = {}
+        argument_keys = kwargs.keys()
+        for property_name, property in self._properties.items():
+            if property_name in argument_keys:
+                self._document[property_name] = kwargs[property_name]
+            else:
+                self._document[property_name] = property.default
+
+
+
+    # @classmethod
+    # def get(cls, ids, rev=None, db=None, dynamic_properties=True):
+    #     """
+    #     Get documents by ids.
+    #     :param ids: {list or string} The documents' id.
+    #     :return: {list or Document}
+    #     """
+    #     if ids is None:
+    #         return None
+    #     if isinstance(ids, list):
+    #         if db is None:
+    #             db = cls.get_db()
+    #         return db.view(
+    #             '%s/id' % db.dbname,
+    #             keys=ids,
+    #             schema=cls
+    #         ).all()
+    #     else:
+    #         try:
+    #             return super(Document, cls).get(ids, rev, db, dynamic_properties)
+    #         except ResourceNotFound:
+    #             return None
 
     @classmethod
     def where(cls, *args, **kwargs):
@@ -78,7 +98,6 @@ class Document(DocumentBase):
         """
         Save the document.
         """
-        super(Document, self).save(**params)
         es = utils.get_elasticsearch()
         es.index(
             index=self.get_db().dbname,
@@ -99,4 +118,3 @@ class Document(DocumentBase):
             id=self._id,
         )
         es.indices.flush()
-        super(Document, self).delete()
--- asahi/properties.py
+++ asahi/properties.py
@@ -1,9 +1,67 @@
 from couchdbkit import schema
 
 
-Property = schema.Property
-StringProperty = schema.StringProperty
-IntegerProperty = schema.IntegerProperty
+class Property(object):
+    def __init__(self, default=None, is_required=False):
+        self.document_class = None
+        self.name = None
+        self.default = default
+        self.is_required=is_required
+
+    def __get__(self, document_instance, document_class):
+        if document_instance is None:
+            return self
+
+        value = document_instance._document.get(self.name)
+        if value is None:
+            return value
+        return self.__to_python(value)
+
+    def __set__(self, document_instance, value):
+        document_instance._document[self.name] = self.__to_json(value)
+
+    def __property_config__(self, document_class, property_name):
+        """
+        Setup the property.
+        :param document_class: {Document} The document class.
+        :param property_name: {string} The property name in the document.
+        :return:
+        """
+        self.document_class = document_class
+        if self.name is None:
+            self.name = property_name
+
+    def __to_python(self, value):
+        """
+        Convert the value to Python format.
+        :param value:
+        :return:
+        """
+        return unicode(value)
+    def __to_json(self, value):
+        """
+        Convert the value to ElasticSearch format.
+        :param value:
+        :return:
+        """
+        if value is None:
+            return value
+        return unicode(value)
+
+class StringProperty(Property):
+    __to_python = unicode
+    __to_json = unicode
+
+class IntegerProperty(Property):
+    __to_python = int
+    __to_json = int
+
+class LongProperty(Property):
+    __to_python = long
+    __to_json = long
+
+
+# Property = schema.Property
 DecimalProperty = schema.DecimalProperty
 BooleanProperty = schema.BooleanProperty
 FloatProperty = schema.FloatProperty
