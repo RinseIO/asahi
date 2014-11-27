cf09d03ad4507d0d90110d605fd3ff6d06cf8acc
Kelp <kelp@phate.org>
2014-11-27 15:31:14 +0800

    updated db.py

--- asahi/db.py
+++ asahi/db.py
@@ -1,2 +1,3 @@
 from asahi.document import Document
-from asahi.properties import *
+from asahi.properties import Property, StringProperty, IntegerProperty, FloatProperty,\
+    BooleanProperty, DateTimeProperty, ListProperty, DictProperty, ReferenceProperty
